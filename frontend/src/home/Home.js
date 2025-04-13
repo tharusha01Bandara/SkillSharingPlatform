2c08a218671d161b2ce5a51fa4a838115cae6836
tharusha01Bandara <154556642+tharusha01Bandara@users.noreply.github.com>
2025-04-14 02:55:31 +0530

    Initial commit

---
+++ frontend/src/home/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="home-container">
+                <div className="container">
+                
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
